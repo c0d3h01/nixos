783c51f588c31b8d7b28aaae584cb3ba8fda7ed4
c0d3h01 <c0d3h01@gmail.com>
2025-03-23 19:40:45 +0530

    upd: optimize Android Nix configuration by removing unused versions and enhancing path exports

--- nix/apps/android.nix
+++ nix/apps/android.nix
@@ -1,27 +1,19 @@
-{ config, pkgs, username, ... }:
-
+{ config, pkgs, ... }:
 let
   androidenv = pkgs.androidenv;
   androidSdk = (androidenv.composeAndroidPackages {
     cmdLineToolsVersion = "8.0";
-    toolsVersion = "26.1.1";
     platformToolsVersion = "35.0.2";
     buildToolsVersions = [ "30.0.3" ];
+    platformVersions = [ "30" ];
     includeEmulator = false;
-    emulatorVersion = "30.3.4";
-    platformVersions = [ "28" "29" "30" ];
     includeSources = false;
     includeSystemImages = false;
-    systemImageTypes = [ "google_apis_playstore" ];
-    abiVersions = [ "armeabi-v7a" "arm64-v8a" ];
-    cmakeVersions = [ "3.10.2" ];
     includeNDK = true;
     ndkVersions = [ "22.0.7026061" ];
   }).androidsdk;
 in
 {
-  nixpkgs.config.android_sdk.accept_license = true;
-
   environment.variables = {
     ANDROID_HOME = "${androidSdk}/libexec/android-sdk";
     ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
@@ -29,15 +21,10 @@ in
     ANDROID_NDK_HOME = "${androidSdk}/libexec/android-sdk/ndk/22.0.7026061";
   };
 
-  environment.systemPackages = with pkgs; [
-    universal-android-debloater # uad-ng
-    android-studio
-    flutter
-    androidSdk
-  ];
-
   environment.extraInit = ''
     export PATH="${androidSdk}/libexec/android-sdk/ndk/22.0.7026061:$PATH"
     export PATH="${androidSdk}/libexec/android-sdk/platform-tools:$PATH"
+    export PATH="${androidSdk}/libexec/android-sdk/cmdline-tools/latest/bin:$PATH"
+    export PATH="${androidSdk}/libexec/android-sdk/build-tools/30.0.3:$PATH"
   '';
 }
