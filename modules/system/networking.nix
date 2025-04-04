1c954ecc4645f962a138d7975147d9a422eeb27c
c0d3h01 <c0d3h01@gmail.com>
2025-04-04 13:58:31 +0530

    feat: networking - enable mDNS

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -1,11 +1,14 @@
 {
   services.resolved.enable = true;
   systemd.network.wait-online.enable = false;
+
   networking = {
     networkmanager = {
       enable = true;
       wifi.powersave = false;
+      connectionConfig."connection.mdns" = 2;
     };
+
     nameservers = [
       "1.1.1.1"
       "1.0.0.1"
