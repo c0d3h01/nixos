b2c1b65e1e10517d1ef9576c80fb776ddae31b3a
c0d3h01 <c0d3h01@gmail.com>
2025-04-07 11:49:22 +0530

    upd: enhance font configuration with additional font packages and improved defaults

--- modules/system/fonts.nix
+++ modules/system/fonts.nix
@@ -7,17 +7,47 @@
     fontDir.enable = true;
     enableDefaultPackages = true;
     packages = with pkgs; [
+      # Main fonts
       dejavu_fonts
       hack-font
       twitter-color-emoji
+      
+      # Programming fonts
+      jetbrains-mono
+      fira-code
+      iosevka
+      source-code-pro
+      
+      # UI fonts
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-emoji
+      roboto
+      open-sans
+      
+      # Icon fonts
+      material-design-icons
+      font-awesome
+      
+      # Nerd fonts (for terminal icons)
+      (nerdfonts.override { fonts = [ "FiraCode" "JetBrainsMono" "Iosevka" ]; })
     ];
     fontconfig = {
       enable = true;
       defaultFonts = {
-        serif = lib.mkForce [ "DejaVu Serif" ];
-        sansSerif = lib.mkForce [ "DejaVu Sans" ];
-        monospace = lib.mkForce [ "Hack" ];
-        emoji = lib.mkForce [ "Twitter Color Emoji" ];
+        serif = lib.mkForce [ "Noto Serif" "DejaVu Serif" ];
+        sansSerif = lib.mkForce [ "Noto Sans" "DejaVu Sans" ];
+        monospace = lib.mkForce [ "JetBrains Mono" "Fira Code" "Hack" ];
+        emoji = lib.mkForce [ "Twitter Color Emoji" "Noto Color Emoji" ];
+      };
+      hinting = {
+        enable = true;
+        style = "slight";
+      };
+      antialias = true;
+      subpixel = {
+        rgba = "rgb";
+        lcdfilter = "default";
       };
     };
   };
