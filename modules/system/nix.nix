6277e5e73c4bf51d92ef7020dc315f5d351ffb48
c0d3h01 <c0d3h01@gmail.com>
2025-04-09 09:49:40 +0530

    feat: disable NixOS documentation generation and enhance Nix settings with trace and keep-going options

--- modules/system/nix.nix
+++ modules/system/nix.nix
@@ -1,8 +1,10 @@
 { config
 , user
 , lib
-, ...
-}: {
+, ... }: {
+
+  # build takes forever
+  documentation.nixos.enable = false;
 
   system.autoUpgrade = {
     enable = true;
@@ -19,20 +21,25 @@
 
   nix = {
     settings = {
+      show-trace = true;
+      keep-going = true;
       auto-optimise-store = true;
       max-jobs = "auto";
       cores = 0; # Use all available cores
       builders = lib.mkForce "";
       trusted-users = [ "root" "${user.username}" ];
+
       experimental-features = [
         "nix-command"
         "flakes"
         "ca-derivations"
       ];
+
       substituters = [
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
       ];
+
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
