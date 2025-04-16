291adb2008f5dbdcce0ea530652fe5c81713a4a5
c0d3h01 <haarshalsawant@gmail.com>
2025-04-16 22:43:09 +0530

    chore: reconfigured all config

--- flake.nix
+++ flake.nix
@@ -1,9 +1,10 @@
 {
-  description = "NixOS Dotfiles c0d3h01";
+  description = "Credit: Harshal (c0d3h01), NixOS Dotfiles";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -39,15 +40,17 @@
       self,
       nixpkgs,
       home-manager,
+      flake-utils,
       ...
     }@inputs:
     let
-      # System Architecture
       supportedSystems = [
-        "x86_64-linux"
         "aarch64-linux"
+        "i686-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
       ];
-      defaultSystem = "x86_64-linux";
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
       # User Configuations
@@ -58,85 +61,41 @@
         hostname = "NixOS";
         stateVersion = "24.11";
       };
-
-      # Special Arguments for Nix modules
-      specialArgs = system: {
-        inherit inputs system userConfig;
-      };
-
-      mkPkgs =
-        system:
-        import nixpkgs {
-          inherit system;
-          config = {
-            allowUnfree = true;
-            tarball-ttl = 0;
-            android_sdk.accept_license = true;
-          };
-        };
-
-      # NixOS Configuration
-      mkNixOSConfiguration =
-        {
-          system ? defaultSystem,
-          hostname ? userConfig.hostname,
-        }:
-        nixpkgs.lib.nixosSystem {
-          inherit system;
-          specialArgs = specialArgs system;
-
-          modules = [
-            ./system
-            { nixpkgs.pkgs = mkPkgs system; }
-
-            # Home Manager integration
-            home-manager.nixosModules.home-manager
-            {
-              home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                extraSpecialArgs = specialArgs system;
-
-                users.${userConfig.username} = {
-                  imports = [ ./home/home.nix ];
-                  home.stateVersion = userConfig.stateVersion;
-                };
-              };
-            }
-          ];
-        };
     in
     {
-      nixosConfigurations.${userConfig.hostname} = mkNixOSConfiguration { };
-
-      devShells = forAllSystems (
-        system:
-        let
-          pkgs = mkPkgs system;
-        in
-        {
-          default = pkgs.mkShell {
-            packages = with nixpkgs; [
-              pkg-config
-              gtk3
-            ];
-            shellHook = "exec zsh";
-          };
-        }
-      );
-
-      checks = forAllSystems (
-        system:
-        inputs.pre-commit-hooks.lib.${system}.run {
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-tree);
+      checks = forAllSystems (system: {
+        pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
           src = ./.;
           hooks = {
-            nixpkgs-fmt.enable = true;
-            statix.enable = true;
-            deadnix.enable = true;
+            alejandra.enable = true;
           };
-        }
-      );
+        };
+      });
 
-      formatter = forAllSystems (system: (mkPkgs system).nixfmt-tree);
+      overlays = import ./overlays { inherit inputs; };
+
+      # NixOS configuration
+      nixosConfigurations = {
+        ${userConfig.hostname} = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs userConfig; };
+          modules = [
+            ./modules/nixos
+            ./hosts/laptop
+          ];
+        };
+      };
+
+      # Standalone home-manager configuration
+      homeConfigurations = {
+        "${userConfig.username}@${userConfig.hostname}" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          extraSpecialArgs = { inherit inputs userConfig; };
+          modules = [
+            ./modules/home-manager/home.nix
+          ];
+        };
+      };
     };
 }
