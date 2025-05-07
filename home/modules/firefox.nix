a2f0c4e25c1d0869a1851518bd5e3976bd98524c
c0d3h01 <harshalsawant.dev@gmail.com>
2025-05-07 12:05:25 +0530

    feat: firefox module with custom settings and userChrome CSS

---
+++ home/modules/firefox.nix
@@ -0,0 +1,125 @@
+{
+  lib,
+  userConfig,
+  pkgs,
+  ...
+}:
+
+{
+  home.sessionVariables.MOZ_USE_XINPUT2 = "1";
+  programs.firefox = lib.mkForce {
+    enable = true;
+    package = pkgs.firefox-esr;
+
+    profiles.${userConfig.username} = {
+      # Essential extensions only
+      extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
+        ublock-origin
+        privacy-badger
+        link-cleaner
+      ];
+
+      settings = {
+        # Core privacy settings
+        "browser.send_pings" = false;
+        "network.cookie.cookieBehavior" = 1;
+        "privacy.resistFingerprinting" = false;
+        "network.http.referer.XOriginPolicy" = 0;
+        "dom.security.https_only_mode_ever_enabled" = true;
+        "geo.enabled" = false;
+
+        # Disable telemetry
+        "toolkit.telemetry.enabled" = false;
+        "toolkit.telemetry.unified" = false;
+        "toolkit.telemetry.server" = "";
+
+        # Disable Pocket
+        "extensions.pocket.enabled" = false;
+
+        # Performance settings
+        "browser.cache.disk.enable" = true;
+        "browser.cache.memory.enable" = true;
+
+        # Media and DRM (for streaming services)
+        "media.eme.enabled" = true;
+        "media.gmp-widevinecdm.enabled" = true;
+
+        # UI preferences
+        "browser.uidensity" = 1;
+        "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        "browser.toolbars.bookmarks.visibility" = "always";
+
+        # Usability settings
+        "browser.search.suggest.enabled" = true;
+        "browser.urlbar.suggest.bookmark" = true;
+        "browser.urlbar.suggest.history" = true;
+        "media.autoplay.enabled" = true;
+      };
+
+      # UserChrome CSS for consistent theming
+      userChrome = "
+          * {
+              box-shadow: none !important;
+              border: 0px solid !important;
+          }
+
+          #tabbrowser-tabs {
+              --user-tab-rounding: 8px;
+          }
+
+          .tab-background {
+              border-radius: var(--user-tab-rounding) var(--user-tab-rounding) 0px 0px !important;
+              margin-block: 1px 0 !important;
+          }
+
+          #scrollbutton-up, #scrollbutton-down {
+              border-top-width: 1px !important;
+              border-bottom-width: 0 !important;
+          }
+
+          .tab-background:is([selected], [multiselected]):-moz-lwtheme {
+              --lwt-tabs-border-color: rgba(0, 0, 0, 0.5) !important;
+              border-bottom-color: transparent !important;
+          }
+
+          [brighttext='true'] .tab-background:is([selected], [multiselected]):-moz-lwtheme {
+              --lwt-tabs-border-color: rgba(255, 255, 255, 0.5) !important;
+              border-bottom-color: transparent !important;
+          }
+
+          /* Container color bar visibility */
+          .tabbrowser-tab[usercontextid] > .tab-stack > .tab-background > .tab-context-line {
+              margin: 0px max(calc(var(--user-tab-rounding) - 3px), 0px) !important;
+          }
+
+          #TabsToolbar, #tabbrowser-tabs {
+              --tab-min-height: 29px !important;
+          }
+
+          #main-window[sizemode='true'] #toolbar-menubar[autohide='true'] + #TabsToolbar,
+          #main-window[sizemode='true'] #toolbar-menubar[autohide='true'] + #TabsToolbar #tabbrowser-tabs {
+              --tab-min-height: 30px !important;
+          }
+
+          #scrollbutton-up,
+          #scrollbutton-down {
+              border-top-width: 0 !important;
+              border-bottom-width: 0 !important;
+          }
+
+          #TabsToolbar, #TabsToolbar > hbox, #TabsToolbar-customization-target, #tabbrowser-arrowscrollbox  {
+              max-height: calc(var(--tab-min-height) + 1px) !important;
+          }
+
+          #TabsToolbar-customization-target toolbarbutton > .toolbarbutton-icon,
+          #TabsToolbar-customization-target .toolbarbutton-text,
+          #TabsToolbar-customization-target .toolbarbutton-badge-stack,
+          #scrollbutton-up,#scrollbutton-down {
+              padding-top: 7px !important;
+              padding-bottom: 6px !important;
+          }
+        ";
+    };
+  };
+}
