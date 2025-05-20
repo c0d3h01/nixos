22aa547528214f84bc24f6c39504fc1d43a96391
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-05-20 20:21:11 +0530

    chore(config): simplify allowUnfreePredicate and remove unused code

--- home/config.nix
+++ home/config.nix
@@ -1,23 +1,9 @@
-{
-  nurFun ? (
-    import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz")
-  ),
-  ...
-}:
+# https://github.com/nix-community/home-manager/issues/2942
+
 {
   allowUnfree = true;
-  # https://github.com/nix-community/home-manager/issues/2942
-  allowUnfreePredicate = _pkg: true;
-
-  # pulseaudio = true;
+  allowUnfreePredicate = true;
   #allowUnsupportedSystem = true;
   oraclejdk.accept_license = true;
   android_sdk.accept_license = true;
-
-  packageOverrides = pkgs: {
-    nur = nurFun {
-      nurpkgs = pkgs;
-      inherit pkgs;
-    };
-  };
 }
