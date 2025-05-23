5bba42acec35a15fc3f4073421c08023325a5330
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-05-23 12:32:40 +0000

    test(f2fs): filesystem disko config

--- machines/installer/disko-config.nix
+++ machines/installer/disko-config.nix
@@ -28,54 +28,72 @@
               content = {
                 type = "swap";
                 discardPolicy = "both";
+                resumeDevice = true;
               };
             };
             root = {
-              name = "nixos-root";
-              size = "100%";
+              name = "root";
+              end = "-0";
               content = {
-                type = "btrfs";
-                extraArgs = [ "-f" ];
-                subvolumes = {
-                  "/@" = {
-                    mountpoint = "/";
-                    mountOptions = [
-                      "compress=zstd:1"
-                      "discard=async"
-                      "noatime"
-                      "ssd"
-                    ];
-                  };
-                  "/@home" = {
-                    mountpoint = "/home";
-                    mountOptions = [
-                      "compress=zstd:1"
-                      "discard=async"
-                      "noatime"
-                      "ssd"
-                    ];
-                  };
-                  "/@nix" = {
-                    mountpoint = "/nix";
-                    mountOptions = [
-                      "compress=zstd:1"
-                      "discard=async"
-                      "noatime"
-                      "ssd"
-                    ];
-                  };
-                  "/@log" = {
-                    mountpoint = "/var/log";
-                    mountOptions = [
-                      "compress=zstd:1"
-                      "discard=async"
-                      "noatime"
-                      "ssd"
-                    ];
-                  };
-                };
+                type = "filesystem";
+                format = "f2fs";
+                mountpoint = "/";
+                extraArgs = [
+                  "-O"
+                  "extra_attr,inode_checksum,sb_checksum,compression"
+                ];
+                # Recommendations for flash: https://wiki.archlinux.org/title/F2FS#Recommended_mount_options
+                mountOptions = [
+                  "compress_algorithm=zstd:6,compress_chksum,atgc,gc_merge,lazytime,nodiscard"
+                ];
               };
             };
+            # root = {
+            #   name = "nixos-root";
+            #   size = "100%";
+            #   content = {
+            #     type = "btrfs";
+            #     extraArgs = [ "-f" ];
+            #     subvolumes = {
+            #       "/@" = {
+            #         mountpoint = "/";
+            #         mountOptions = [
+            #           "compress=zstd:1"
+            #           "discard=async"
+            #           "noatime"
+            #           "ssd"
+            #         ];
+            #       };
+            #       "/@home" = {
+            #         mountpoint = "/home";
+            #         mountOptions = [
+            #           "compress=zstd:1"
+            #           "discard=async"
+            #           "noatime"
+            #           "ssd"
+            #         ];
+            #       };
+            #       "/@nix" = {
+            #         mountpoint = "/nix";
+            #         mountOptions = [
+            #           "compress=zstd:1"
+            #           "discard=async"
+            #           "noatime"
+            #           "ssd"
+            #         ];
+            #       };
+            #       "/@log" = {
+            #         mountpoint = "/var/log";
+            #         mountOptions = [
+            #           "compress=zstd:1"
+            #           "discard=async"
+            #           "noatime"
+            #           "ssd"
+            #         ];
+            #       };
+            #     };
+            #   };
+            # };
           };
         };
       };
