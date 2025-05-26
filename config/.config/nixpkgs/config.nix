e73cad53c3c2e42636d0b9adcef296fd42a74b44
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-05-26 17:02:51 +0530

    chore: update flake.lock and remove obsolete config.nix

--- config/.config/nixpkgs/config.nix
+++ config/.config/nixpkgs/config.nix
@@ -1,1 +1,8 @@
-../../../home/config.nix
+{
+  allowUnfree = true;
+  allowUnfreePredicate = true;
+  #allowUnsupportedSystem = true;
+  oraclejdk.accept_license = true;
+  android_sdk.accept_license = true;
+}
+
--- home/config.nix
+++
@@ -1,9 +0,0 @@
-# https://github.com/nix-community/home-manager/issues/2942
-
-{
-  allowUnfree = true;
-  allowUnfreePredicate = true;
-  #allowUnsupportedSystem = true;
-  oraclejdk.accept_license = true;
-  android_sdk.accept_license = true;
-}
