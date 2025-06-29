cdee23e445be9e6190efd9f42f168a595f9b7b2f
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-06-29 16:04:12 +0530

    feat: add encrypted secrets for element-key and passwd

--- secrets/default.nix
+++ secrets/default.nix
@@ -7,18 +7,23 @@
 {
   sops = {
     age = {
-      keyFile = "${config.home.homeDirectory}/.config/sops/sops-secrets-key.txt";
+      keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
       sshKeyPaths = [
         "${config.home.homeDirectory}/.ssh/id_ed25519"
       ];
     };
 
     secrets = {
-      # "element" = {
-      #   sopsFile = ;
-      #   path = "/${config.home.homeDirectory}/.secrets/passwd";
-      #   format = "binary";
-      # }
+      "passwd" = {
+        sopsFile = ./c0d3h01/passwd.enc;
+        path = "/${config.home.homeDirectory}/.config/sops/age/passwd";
+        format = "binary";
+      };
+      "element-key" = {
+        sopsFile = ./c0d3h01/element-key.enc;
+        path = "/${config.home.homeDirectory}/.config/sops/age/element-key";
+        format = "binary";
+      };
     };
   };
 }
