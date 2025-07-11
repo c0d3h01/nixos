8a2b5466f3ee1d7ee88463d994531fbdd7ebff5f
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-07-11 11:55:36 +0530

    shift fonts settings to hm

---
+++ homeManager/modules/services/fonts.nix
@@ -0,0 +1,32 @@
+{
+  pkgs,
+  ...
+}:
+
+{
+  home.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-emoji
+    nerd-fonts.jetbrains-mono
+  ];
+
+  fonts.fontconfig = {
+    enable = true;
+
+    defaultFonts = {
+      serif = [
+        "Noto Serif"
+      ];
+      sansSerif = [
+        "Noto Sans"
+      ];
+      monospace = [
+        "JetBrainsMono Nerd Font"
+      ];
+      emoji = [
+        "Noto Color Emoji"
+      ];
+    };
+  };
+}
