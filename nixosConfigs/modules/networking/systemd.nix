99baff5b05d99937f5040202fc3c1512da4472e4
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-08-06 12:55:20 +0530

    refactor: systemd networking by removing unused device handling

--- nixosConfigs/modules/networking/systemd.nix
+++ nixosConfigs/modules/networking/systemd.nix
@@ -1,12 +1,3 @@
-{ lib, ... }:
-let
-  inherit (lib) mkForce concatMapAttrs genAttrs;
-
-  ethernetDevices = [
-    "wlp1s0f0u8" # wifi dongle
-    "enp7s0" # ethernet interface on the motherboard
-  ];
-in
 {
   # systemd DNS resolver daemon
   services.resolved.enable = true;
@@ -22,13 +13,6 @@ in
       # also prevents failures from services that are restarted instead of stopped
       systemd-networkd.stopIfChanged = false;
       systemd-resolved.stopIfChanged = false;
-    }
-    // concatMapAttrs (_: v: v) (
-      genAttrs ethernetDevices (device: {
-        # Assign an IP address when the device is plugged in rather than on startup. Needed to prevent
-        # blocking the boot sequence when the device is unavailable, as it is hotpluggable.
-        "network-addresses-${device}".wantedBy = mkForce [ "sys-subsystem-net-devices-${device}.device" ];
-      })
-    );
+    };
   };
 }
