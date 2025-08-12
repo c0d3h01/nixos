69c8effa15e26fc2bfab3f60607fc848f27d690e
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-08-12 11:23:55 +0530

    chore(host): more clean look

--- systems/c0d3h01/default.nix
+++ systems/c0d3h01/default.nix
@@ -6,6 +6,7 @@
 let
   # Check if current user is c0d3h01
   isC0d3h01 = userConfig.username == "c0d3h01";
+  ssh-keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICSjL8HGjiSAnLHupMZin095bql7A8+UDfc7t9XCZs8l" ];
 in
 {
   imports = [
@@ -16,17 +17,13 @@ in
     root = {
       # Allow the user to log in as root without a password.
       hashedPassword = "";
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICSjL8HGjiSAnLHupMZin095bql7A8+UDfc7t9XCZs8l"
-      ];
+      openssh.authorizedKeys.keys = ssh-keys;
     };
 
     c0d3h01 = {
       home = "/home/c0d3h01";
       hashedPassword = "$y$j9T$zv/9zYffWILQWXz9xwMaa0$oKN.JemKWm/KA4p.mO3rzSIS.ODD7jQeeG5NbvQ0Wa5";
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICSjL8HGjiSAnLHupMZin095bql7A8+UDfc7t9XCZs8l"
-      ];
+      openssh.authorizedKeys.keys = ssh-keys;
     };
   };
 }
