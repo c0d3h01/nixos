909bee1f1d0ca1e56501646288303a18de02a8a1
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-08-12 10:53:09 +0530

    refactored(system): seperated locale & xserver from base config

--- nixosConfigs/modules/system/base.nix
+++ nixosConfigs/modules/system/base.nix
@@ -6,15 +6,18 @@
 }:
 
 {
-  # Basic system configuration that applies to all machines
-
   # Set hostname
   networking.hostName = userConfig.hostname;
 
   # System state version
   system.stateVersion = lib.trivial.release;
 
+  # Enable sudo for wheel group
+  security.sudo.wheelNeedsPassword = false;
+
+  # Zsh program enabled as default user
   programs.zsh.enable = true;
+
   # Create the main user
   users.users.${userConfig.username} = {
     uid = lib.mkDefault 1000;
@@ -39,41 +42,5 @@
       "mysql"
       "cloudflared"
     ];
-  };
-
-  # Enable sudo for wheel group
-  security.sudo.wheelNeedsPassword = false;
-
-  # Configure X11
-  services.xserver = lib.mkIf userConfig.machineConfig.workstation {
-    enable = true;
-    xkb = {
-      layout = "us";
-      variant = ""; # Standard QWERTY
-      options = "grp:alt_shift_toggle";
-    };
-
-    # Drivers will be detected & set itself
-    videoDrivers = [
-      "modesetting"
-      "fbdev"
-    ];
-  };
-
-  # Timezone and locale - INDIAN timing
-  time.timeZone = "Asia/Kolkata";
-  i18n = {
-    defaultLocale = "en_IN";
-    extraLocaleSettings = {
-      LC_ADDRESS = "en_IN";
-      LC_IDENTIFICATION = "en_IN";
-      LC_MEASUREMENT = "en_IN";
-      LC_MONETARY = "en_IN";
-      LC_NAME = "en_IN";
-      LC_NUMERIC = "en_IN";
-      LC_PAPER = "en_IN";
-      LC_TELEPHONE = "en_IN";
-      LC_TIME = "en_IN";
-    };
   };
 }
