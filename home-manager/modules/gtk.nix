3a0061cefb17eec807b9edcdfdfbdfe421500d34
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-08-16 14:36:57 +0530

    refactor(gtk): streamline GTK configuration and update theme settings

--- home-manager/modules/gtk.nix
+++ home-manager/modules/gtk.nix
@@ -6,48 +6,38 @@
 }:
 
 {
-  config = {
-    home.pointerCursor = {
-      gtk.enable = true;
-      x11.enable = true;
+  home.pointerCursor = {
+    gtk.enable = true;
+    x11.enable = true;
+    name = "Bibata-Modern-Ice";
+    package = pkgs.bibata-cursors;
+    size = 24;
+  };
+
+  # GTK theming
+  gtk = {
+    enable = true;
+
+    # Set system UI font
+    font = {
+      name = "Inter";
+      size = 10;
+    };
+
+    theme = {
+      name = "Orchis-Dark";
+      package = pkgs.orchis-theme;
+    };
+
+    iconTheme = {
+      name = "Tela-circle";
+      package = pkgs.tela-circle-icon-theme;
+    };
+
+    cursorTheme = {
       name = "Bibata-Modern-Ice";
       package = pkgs.bibata-cursors;
       size = 24;
-    };
-
-    # GTK theming
-    gtk = {
-      enable = true;
-
-      # Set system UI font
-      font = {
-        name = "Inter";
-        size = 10;
-      };
-
-      theme = {
-        name = "Adwaita-Dark";
-        package = pkgs.gnome-themes-extra;
-      };
-
-      iconTheme = {
-        name = "Adwaita";
-        package = pkgs.gnome-themes-extra;
-      };
-
-      cursorTheme = {
-        name = "Bibata-Modern-Ice";
-        package = pkgs.bibata-cursors;
-        size = 24;
-      };
-
-      gtk3.extraConfig = {
-        gtk-application-prefer-dark-theme = true;
-      };
-
-      gtk4.extraConfig = {
-        gtk-application-prefer-dark-theme = true;
-      };
     };
   };
 }
