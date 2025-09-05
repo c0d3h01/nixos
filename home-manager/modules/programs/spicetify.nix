3f858c05a0918243a16cd85c7f94c907a1122634
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-09-05 20:04:28 +0530

    fix: correct mkIf workstation

--- home-manager/modules/programs/spicetify.nix
+++ home-manager/modules/programs/spicetify.nix
@@ -11,7 +11,7 @@
     inputs.spicetify.homeManagerModules.default
   ];
 
-  programs.spicetify = lib.mkIf userConfig.devStack.monitoring.enable (
+  programs.spicetify = lib.mkIf userConfig.machineConfig.workstation.enable (
     let
       spicePkgs = inputs.spicetify.legacyPackages.${pkgs.stdenv.hostPlatform.system};
     in
