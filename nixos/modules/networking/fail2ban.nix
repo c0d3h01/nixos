fd93f0356b06c498edfadfccf512deea02c732ff
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-09-06 11:21:11 +0530

    refactor(fail2ban): update banaction and optimize bantime-increment settings

--- nixos/modules/networking/fail2ban.nix
+++ nixos/modules/networking/fail2ban.nix
@@ -1,21 +1,42 @@
 {
-  # fail2ban firewall jail
   services.fail2ban = {
     enable = true;
-    banaction = "iptables-multiport[blocktype=DROP]";
-    maxretry = 7;
+    banaction = "nftables-multiport";
     ignoreIP = [
       "127.0.0.0/8"
+      "::1"
       "10.0.0.0/8"
+      "172.16.0.0/12"
       "192.168.0.0/16"
+      "169.254.0.0/16"
+      "fc00::/7"
+      "fe80::/10"
     ];
 
     bantime-increment = {
       enable = true;
-      rndtime = "12m";
       overalljails = true;
-      multipliers = "4 8 16 32 64 128 256 512 1024 2048";
-      maxtime = "192h";
+      rndtime = "5m";
+      multipliers = "2 4 8 16 32 64 128 256";
+      maxtime = "168h";
+    };
+
+    jails = {
+      DEFAULT = {
+        settings = {
+          findtime = "10m";
+        };
+      };
+
+      sshd = {
+        settings = {
+          enable = true;
+          port = "ssh";
+          filter = "sshd";
+          backend = "systemd";
+          logpath = "%(sshd_log)s";
+        };
+      };
     };
   };
 }
