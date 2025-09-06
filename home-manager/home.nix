227568d6c55a0d5221c2b42bd803c624ab5533f7
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-09-06 13:56:20 +0530

    chore: disabled man pages of home

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -12,6 +12,9 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
+  manual.manpages.enable = false;
+  programs.man.enable = lib.mkDefault false;
+
   home = {
     inherit (userConfig) username;
     homeDirectory = "/home/${userConfig.username}";
