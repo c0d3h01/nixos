6fb33d046558f74f06cd24dc17e1465f5f66f1e3
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-01-27 22:46:25 +0530

    rm : htop config

--- home-manager/modules/packages.nix
+++ home-manager/modules/packages.nix
@@ -1,7 +1,5 @@
 {
-  userConfig,
   pkgs,
-  config,
   ...
 }: {
   home.packages = with pkgs; [
@@ -23,39 +21,8 @@
     tmux
     fzf
     mise
-    direnv
-    nix-direnv
     sapling
     watchman
     gemini-cli
   ];
-
-  programs.htop = {
-    enable = true;
-    settings =
-      {
-        tree_view = 1;
-        hide_kernel_threads = 1;
-        hide_userland_threads = 1;
-        shadow_other_users = 1;
-        show_thread_names = 1;
-        show_program_path = 0;
-        highlight_base_name = 1;
-        header_layout = "two_67_33";
-        color_scheme = 6;
-      }
-      // (with config.lib.htop; leftMeters [(bar "AllCPUs4")])
-      // (
-        with config.lib.htop;
-          rightMeters [
-            (bar "Memory")
-            (bar "Swap")
-            (bar "DiskIO")
-            (bar "NetworkIO")
-            (text "Systemd")
-            (text "Tasks")
-            (text "LoadAverage")
-          ]
-      );
-  };
 }
