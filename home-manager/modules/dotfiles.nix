a4e0edf80f5cb8c38eae74c4855dfd236e688cdc
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-01-27 22:45:34 +0530

    refactored: dotfiles activation script

--- home-manager/modules/dotfiles.nix
+++ home-manager/modules/dotfiles.nix
@@ -10,12 +10,8 @@
     REPO_URL="https://github.com/c0d3h01/dotfiles.git"
 
     if [ ! -d "$DOTFILES_DIR" ]; then
-      echo "Dotfiles directory not found. Cloning and stowing..."
-      # Clone the repo
-      $DRY_RUN_CMD ${pkgs.git}/bin/git clone --recurse-submodules "$REPO_URL" "$DOTFILES_DIR"
-      $DRY_RUN_CMD ${pkgs.stow}/bin/stow -d "$DOTFILES_DIR" -t "${config.home.homeDirectory}" .
-    else
-      echo "Dotfiles directory exists. Skipping clone and stow."
+      ${pkgs.git}/bin/git clone --recurse-submodules "$REPO_URL" "$DOTFILES_DIR"
+      ${pkgs.stow}/bin/stow -d "$DOTFILES_DIR" -t "${config.home.homeDirectory}" .
     fi
   '';
 }
